21697cce1a6909747077bc5074e7e61ac78e8c98
W3r5l3y <102gaminginbox@gmail.com>
2023-12-10 02:09:55 +0000

    completed mp game loop

--- mp_game_engine.py
+++ mp_game_engine.py
@@ -16,12 +16,12 @@ def generate_attack(size):
 def ai_opponent_game_loop():
     """Manual testing game loop for MP"""
 
-    size = 10  # Size of grid used
+    size = 5  # Size of grid used
 
     print("|" + "-" * 100 + "|")
     print("Wagwan G. Battleships innit (MULTIPLAYER)")
     print("|" + "-" * 100 + "|")
-    username = str(input("Enter your name..."))
+    username = str(input("Enter your name...\n"))
 
     # Initialises the boards and battleships for player and BOT
     player_board = components.initialise_board(size=size)
@@ -47,12 +47,74 @@ def ai_opponent_game_loop():
     players["BOT"]["board"] = components.place_battleships(
         bot_board, bot_battleships, placement="random"
     )
-    # Processes the players coordinates on the BOT's board
-    player_attack = game_engine.cli_coordinates_input()
-    game_engine.attack(player_attack, bot_board, bot_battleships)
+
+    # Game loop
+    game_over = False
+    while game_over is False:
+        # Processes the players coordinates on the BOT's board
+        print("\nYour move.")
+        player_attack = game_engine.cli_coordinates_input()
+        outcome = game_engine.attack(
+            player_attack, players["BOT"]["board"], players["BOT"]["battleships"]
+        )
+        if outcome is True:
+            print("HIT!")
+        else:
+            print("MISS!")
+
+        # Checks if the BOT's board is empty
+        game_over = all(
+            all(value is None for value in row) for row in players["BOT"]["board"]
+        )
+        if game_over is True:
+            winner = username.upper()
+            break
+
+        # Processes the BOT's coordinates on the players board
+        print("\nBot's move.")
+        bot_attack = generate_attack(size)
+        outcome = game_engine.attack(
+            bot_attack, players[username]["board"], players[username]["battleships"]
+        )
+        if outcome is True:
+            print("BOT HIT!")
+        else:
+            print("BOT MISS!")
+        # Prints out players resulting board
+        for row in players[username]["board"]:
+            for cell in row:
+                if cell is None:
+                    print("  ~".ljust(7), end="")
+                else:
+                    print(f" {cell} ".ljust(7), end="")
+            print()
+        # Checks if the player's board is empty
+        game_over = all(
+            all(value is None for value in row) for row in players[username]["board"]
+        )
+        if game_over is True:
+            winner = "BOT"
+
+    # Game over message
+    print("GAME OVER!")
+    print(f"THE WINNER WAS {winner}!")
 
 
 # players = {"james": {"board": [], "battleships": {}}}
 players = {}
 if __name__ == "__main__":
     ai_opponent_game_loop()
+
+"""
+game_over = False
+    while game_over is False:
+        coordinates = cli_coordinates_input()
+        if attack(coordinates, board, battleships) is True:
+            print("HIT!")
+        else:
+            print("MISS!")
+        for lines in board:
+            print(lines)
+        # Checks if the board is empty (all values are None)
+        game_over = all(all(value is None for value in row) for row in board)
+"""
