50e7fc973b8572356c5beb2d05e119eacb747968
W3r5l3y <102gaminginbox@gmail.com>
2023-12-10 10:38:37 +0000

    imported and tested flask

--- main.py
+++ main.py
@@ -2,6 +2,22 @@
 # import components
 # import game_engine
 # import mp_game_engine
+from flask import Flask
+
+# Initialise the Flask object
+app = Flask(__name__)
+
+
+@app.route("/")  # Maps a function to the URL path
+def hello_world():
+    return "Hello World!"
+
+
+@app.route("/cheese")  # Maps a function to the URL path /cheese
+def james_may():
+    return "BEANS!"
+
 
 if __name__ == "__main__":
-    pass
+    # start the thread listening for requests
+    app.run()
