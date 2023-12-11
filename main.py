e49837f937d06d2a23417bd7040662ade4cf0ff9
W3r5l3y <102gaminginbox@gmail.com>
2023-12-11 12:31:19 +0000

    added lecture flask code

--- main.py
+++ main.py
@@ -1,25 +1,63 @@
 """Main module for project"""
-# import components
+
+from flask import Flask, render_template, jsonify, request
+import components
+
 # import game_engine
 # import mp_game_engine
-from flask import Flask
 
 # Initialise the Flask object
 app = Flask(__name__)
 
 
-@app.route("/")  # Maps a function to the URL path
-def hello_world():
-    """Sends Hello World!"""
-    return "Hello World!"
+@app.route("/placement", methods=["GET", "POST"])
+def placement_interface():
+    """Initial placement of the ships on the board"""
+    ships = components.create_battleships()
+    board_size = 10
+
+    if request.method == "GET":
+        return render_template("placement.html", ships=ships, board_size=board_size)
+
+    if request.method == "POST":
+        data = request.get_json()
+        print(data)
+        return jsonify({"Success": True})
+
+    return None
 
 
-@app.route("/cheese")  # Maps a function to the URL path /cheese
-def james_may():
-    """Sends a cheese string"""
-    return "BEANS!"
+@app.route("/", methods=["GET"])
+def root():
+    """PLACEHOLDER"""
+    board = components.initialise_board()
+    if request.method == "GET":
+        return 404
+
+    return render_template("main.html", player_board=board)
+
+
+@app.route("/attack", methods=["GET"])
+def process_attack():
+    """PLACEHOLDER"""
+    game_over = False
+    if request.args:
+        x = request.args.get("x")
+        y = request.args.get("y")
+        print(x, y)
+        if game_over is True:
+            return jsonify(
+                {
+                    "hit": True,
+                    "Player_Turn": (x, y),
+                    "AI_Turn": (1, 2),
+                    "finished": "GAME OVER",
+                }
+            )
+        return jsonify({"hit": True, "Player_Turn": (x, y), "AI_Turn": (1, 2)})
+    return None
 
 
 if __name__ == "__main__":
-    # start the thread listening for requests
+    # Runs the Flask app
     app.run()
